75f45bc42caa633ad03bab092bab557c01d35210
dnynurakhmad99 <80521202+dnynurakhmad99@users.noreply.github.com>
2022-07-06 22:29:56 +0700

    Add files via upload

---
+++ LatihanWPF/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LatihanWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            ResizeMode = ResizeMode.NoResize;
+            txtUsername.Focus();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if(txtUsername.Text == "danny" && txtPassword.Text == "amikom")
+            {
+                MessageBox.Show("Selamat datang!");
+                Window1 winSatu = new Window1();
+                winSatu.Show();
+                this.Close();
+            }
+
+            else
+            {
+                MessageBox.Show("Maaf username atau password salah!");
+                txtUsername.Text = "";
+                txtPassword.Text = "";
+                txtUsername.Focus();
+            }
+        }
+
+        private void txtUsername_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtUsername.SelectionStart = 0;
+            txtUsername.SelectionLength = txtUsername.Text.Length;
+        }
+
+        private void txtPassword_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtPassword.SelectionStart = 0;
+            txtPassword.SelectionLength = txtPassword.Text.Length;
+        }
+    }
+}
